a7d5f048640c8afaf6b5a1a4bfb6959492f4ffae
Andoni Sooklaris <andonis@apple.com>
2021-07-29 18:08:10 -0500

    Integrated versioneer

--- imessage_extractor/__init__.py
+++ imessage_extractor/__init__.py
@@ -3,3 +3,7 @@
 __author__ = """Andoni Sooklaris"""
 __email__ = 'andoni.sooklaris@gmail.com'
 __version__ = '0.1.0'
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
