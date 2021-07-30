92a13913843a636c2d7b3f1d7f9769d160285802
Andoni Sooklaris <andonis@apple.com>
2021-07-30 00:46:49 -0500

    Modified CLI

--- imessage_extractor/cli.py
+++ imessage_extractor/cli.py
@@ -1,16 +1,19 @@
-"""Console script for imessage_extractor."""
-import sys
+import logging
 import click
+import pydoni
+from .src.run import run
 
 
-@click.command()
+@click.group()
+def cli():
+    """
+    Command line interface for imessage-extractor.
+    """
+    pass
+
+
+cli.add_command(run)
+
+
 def main(args=None):
-    """Console script for imessage_extractor."""
-    click.echo("Replace this message by putting your code into "
-               "imessage_extractor.cli.main")
-    click.echo("See click documentation at https://click.palletsprojects.com/")
-    return 0
-
-
-if __name__ == "__main__":
-    sys.exit(main())  # pragma: no cover
+    cli()
