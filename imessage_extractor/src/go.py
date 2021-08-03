a09a7d79e0d751a0a567d375164f154c8d497a5b
Andoni Sooklaris <andonis@apple.com>
2021-08-03 11:03:55 -0500

    Alternative load method

--- imessage_extractor/src/go.py
+++ imessage_extractor/src/go.py
@@ -1,12 +1,14 @@
 import sqlite3
 import click
+import shutil
 import time
 import pydoni
 import logging
 from .verbosity import print_startup_message, logger_setup, path, bold
 from os import makedirs
 from os.path import expanduser, isfile, isdir
-from .extract_sqlite import extract_sqlite
+from .objects import ChatDbExtract
+from .save_to_postgres import save_to_postgres
 
 
 logger = logger_setup(name='imessage-extractor', level=logging.ERROR)
@@ -20,19 +22,35 @@ logger = logger_setup(name='imessage-extractor', level=logging.ERROR)
               help='Path to folder to save chat.db tables to.')
 @click.option('--save-pg-schema', type=str, default=None, required=False,
               help='Name of Postgres schema to save tables to.')
-@click.option('--pg-credentials', type=str, default='~/.pgpass', required=False,
+@click.option('--pg-credentials', type=str, default=expanduser('~/.pgpass'), required=False,
               help=pydoni.advanced_strip("""EITHER the path to a local Postgres credentials
               file 'i.e. ~/.pgpass', OR a string with the connection credentials. Must
               be in format 'hostname:port:db_name:user:pg_pass'."""))
+@click.option('--rebuild', is_flag=True, default=False,
+              help='If saving to Postgres, fully rebuild tables from chat.db source.')
 
 @click.command()
-def go(chat_db_path, verbose, save_csv, save_pg_schema, pg_credentials):
+def go(chat_db_path,
+       verbose,
+       save_csv,
+       save_pg_schema,
+       pg_credentials,
+       rebuild):
     """
     Run the imessage-extractor.
     """
     # Configure logger
     logging_level = logging.INFO if verbose else logging.ERROR
     logger = logger_setup(name='imessage-extractor', level=logging_level)
+
+    # Validate parameters
+    if save_csv is None:
+        if save_pg_schema is None:
+            raise ValueError('Must specify either --save-csv or --save-pg-schema')
+
+    if (save_pg_schema is not None and pg_credentials is None) \
+        or (save_pg_schema is None and pg_credentials is not None):
+            raise ValueError('Must specify both --pg-credentials and --save-pg-schema if one is specified')
 
     # Log parameter values
     params = {k: v for k, v in locals().items() if k not in ['logger', 'logging_level']}
@@ -45,7 +63,7 @@ def go(chat_db_path, verbose, save_csv, save_pg_schema, pg_credentials):
     if verbose:
         print_startup_message(logger)
 
-    logger.info(f"->->->->->->->->->->->->-> {click.style('Extract', bold=True)} <-<-<-<-<-<-<-<-<-<-<-<-<-")
+    # logger.info(f"->->->->->->->->->->->->-> {click.style('Extract', bold=True)} <-<-<-<-<-<-<-<-<-<-<-<-<-")
 
     # Connect to the local chat.db
     if isfile(chat_db_path):
@@ -60,23 +78,22 @@ def go(chat_db_path, verbose, save_csv, save_pg_schema, pg_credentials):
     else:
         raise FileNotFoundError(f'The chat.db file expected at {path(chat_db_path)} could not be found')
 
-    # Returns a `ChatDbExtract` object containing a list of `ChatDbTable` objects
-    chat_db_extract = extract_sqlite(logger=logger, sqlite_con=sqlite_con)
 
-    logger.info(f"->->->->->->->->->->->->-> {click.style('Save', bold=True)} <-<-<-<-<-<-<-<-<-<-<-<-<-")
+    # Create a temporary folder to save the extracted data to if the user opted not
+    # to save the outputted .csv files to a local folder
+    save_csv_dpath = expanduser('~/Desktop/.tmp_imessage_extractor') if save_csv is None else save_csv
+    if not isdir(save_csv_dpath):
+        makedirs(save_csv_dpath)
+        logger.info(f'Created temporary export directory {path(save_csv_dpath)}')
 
-    # Save tables to local CSV folder
-    if save_csv is not None:
-        save_csv = expanduser(save_csv)
-        if not isdir(save_csv):
-            makedirs(save_csv)
+    # Extract metadata for each table in chat.db
+    chat_db_extract = ChatDbExtract(sqlite_con, save_pg_schema, logger)
 
-        logger.info(f'Saving tables as CSV files at path {path(save_csv)}')
-        chat_db_extract.save_to_csv(dir_name=save_csv, verbose=verbose, logger=logger)
-    else:
-        logger.info('User opted not to save tables as local CSV files')
+    # Save tables to .csv files
+    logger.info(f'Saving tables to {path(save_csv_dpath)}')
+    chat_db_extract.save_to_csv(save_csv_dpath, logger)
 
-    # Save tables to Postgres database
+    # Save tables to Postgres
     if save_pg_schema is not None:
         if isfile(expanduser(pg_credentials)):
             with open(expanduser(pg_credentials), 'r') as f:
@@ -94,12 +111,40 @@ def go(chat_db_path, verbose, save_csv, save_pg_schema, pg_credentials):
         pg = pydoni.Postgres(hostname=hostname,
                              port=port,
                              db_name=db_name,
-                             user=user,
+                             pg_user=user,
                              pw=pw)
 
         logger.info(f'Connected to Postgres database {bold(db_name)} hosted on {bold(hostname + ":" + port)}')
 
+        logger.info(f'Saving tables to schema {bold(save_pg_schema)}')
+        chat_db_extract.save_to_postgres(pg, save_pg_schema, logger)
+
+        # save_to_postgres(pg=pg,
+        #                  chat_db_extract=chat_db_extract,
+        #                  pg_schema=save_pg_schema,
+        #                  rebuild=rebuild,
+        #                  logger=logger)
 
     else:
         logger.info('User opted not to save tables to a Postgres database')
 
+
+
+    # logger.info(f"->->->->->->->->->->->->-> {click.style('Save', bold=True)} <-<-<-<-<-<-<-<-<-<-<-<-<-")
+
+    # Save tables to local CSV folder. This must be done regardless of the user's preference
+    # on whether to finally save the data to CSV files or not, since this is an intermediate
+    # step between the SQLite extraction and saving to Postgres.
+    # save_csv = expanduser(save_csv)
+    # if not isdir(save_csv):
+    #     makedirs(save_csv)
+
+    # logger.info(f'Saving tables as CSV files at path {path(save_csv)}')
+    # chat_db_extract.save_to_csv(dir_name=save_csv, verbose=verbose, logger=logger)
+
+
+
+
+    if save_csv is None:
+        shutil.rmtree(save_csv_dpath)
+        logger.info(f'Removed {path(save_csv_dpath)}')
--- imessage_extractor/src/objects.py
+++ imessage_extractor/src/objects.py
@@ -1,79 +1,168 @@
 import click
 import logging
 import pandas as pd
+import json
+import re
 import sqlite3
-from os.path import join
+from pydoni import Postgres
+from os.path import join, dirname, isfile
 from os import stat
 import pydoni
-from .verbosity import bold
+from .verbosity import bold, path
+import typing
+
+
+table_info_json_fpath = join(dirname(__file__), 'table_info.json')
 
 
 class ChatDbTable(object):
     """
-    Store data from a single table in chat.db.
+    Store metadata from a single table in chat.db.
     """
-    def __init__(self, df: pd.DataFrame, sqlite_table_name: str) -> None:
-        self.df = df
-        self.shape = df.shape
-        self.sqlite_table_name = sqlite_table_name
+    def __init__(self,
+                 sqlite_con: sqlite3.Connection,
+                 table_name: str,
+                 write_mode: str,
+                 primary_key: typing.Union[str, list],
+                 pg_schema: str) -> None:
+        self.sqlite_con = sqlite_con
+        self.sqlite_cursor = self.sqlite_con.cursor()
+        self.table_name = table_name
+        self.write_mode = write_mode
+        self.primary_key = primary_key
+        self.pg_schema = pg_schema
+        self.shape = self.get_shape()
+        self.create_sql = self.get_create_sql()
+        self.csv_fpath = None
+
+    def get_create_sql(self) -> str:
+        """
+        Query SQLite for the table creation statement.
+        """
+        # Get create SQL in SQLite dialect
+        query_sql = f"SELECT sql FROM sqlite_master WHERE type = 'table' AND name = '{self.table_name}'"
+        self.sqlite_cursor.execute(query_sql)
+        create_sql_str = [x[0] for x in self.sqlite_cursor.fetchall()][0]
+
+        # Translate SQLite format to Postgres format
+        create_sql_str = create_sql_str.replace('BLOB', 'TEXT')
+        create_sql_str = create_sql_str.replace('INTEGER PRIMARY KEY AUTOINCREMENT', 'SERIAL PRIMARY KEY')
+
+        # Add schema name, and quote both the schema name and table name
+        create_sql_str = re.sub(r'(CREATE TABLE) (.*?) ', fr'\1 "{self.pg_schema}"."\2" ', create_sql_str)
+
+        return create_sql_str
+
+    def get_shape(self) -> tuple:
+        """
+        Query SQLite for the table's shape in format `(rows, columns)`.
+        """
+        self.sqlite_cursor.execute(f'PRAGMA table_info({self.table_name})')
+        n_cols = len(self.sqlite_cursor.fetchall())
+
+        self.sqlite_cursor.execute(f'SELECT COUNT(*) FROM {self.table_name}')
+        n_rows = self.sqlite_cursor.fetchall()[0][0]
+
+        return (n_rows, n_cols)
 
     def save_to_csv(self, file_name: str) -> None:
         """
-        Save table to a csv file.
+        Save table to a .csv file.
         """
-        self.df.to_csv(file_name, index=False)
+        df = pd.read_sql(f'SELECT * FROM {self.table_name}', self.sqlite_con)
+        df.to_csv(file_name, index=False)
+        self.csv_fpath = file_name
+
+    def save_to_postgres(self, pg: Postgres, pg_schema: str) -> None:
+        """
+        Save table to Postgres.
+        """
+        assert isfile(self.csv_fpath), \
+            f'Must create {path(self.csv_fpath)} before inserting to Postgres table {bold(self.table_name)}'
+
+        # Create table
+        pg.drop_table_if_exists(pg_schema, self.table_name)
+        pg.execute(self.create_sql)
+
+        # Save table to Postgres
+        load_sql = pydoni.advanced_strip(f"""
+        COPY {pg_schema}.{self.table_name}
+        FROM '{self.csv_fpath}'
+        (DELIMITER ',',
+        FORMAT CSV,
+        HEADER)
+        """)
+        pg.execute(load_sql)
 
 
 class ChatDbExtract(object):
     """
     Store dictionary of `ChatDbTable` objects.
     """
-    def __init__(self) -> None:
-        self.table_objects = {}
+    def __init__(self, sqlite_con, pg_schema: str, logger: logging.Logger) -> None:
+        self.sqlite_con = sqlite_con
+        self.pg_schema = pg_schema
+        self.logger = logger
+        self.table_objects = self.extract()
 
-    def add_table(self, table_name: str, table_object: ChatDbTable) -> None:
+    def extract(self) -> dict:
         """
-        Append a ChatDbTable object to `self.table_objects`.
+        Query SQLite database for iMessage tables
         """
-        self.table_objects[table_name] = table_object
+        sqlite_cursor = self.sqlite_con.cursor()
 
-    def save_to_csv(self, dir_name: str, verbose: bool, logger: logging.Logger) -> None:
+        # Get full list of SQLite tables in DB
+        sqlite_cursor.execute("SELECT name FROM sqlite_master WHERE type = 'table';")
+        sqlite_table_names = [x[0] for x in sqlite_cursor.fetchall()]
+
+        # Read table data stored as local JSON
+        with open(table_info_json_fpath) as f:
+            table_info = json.load(f)
+
+        # Cross reference queried SQLite tables with those in table_info.json. If a table is
+        # found in SQLite but not in table_info.json or the reverse, then an error is thrown.
+
+        for table_name in sqlite_table_names:
+            if table_name not in table_info.keys():
+                raise Exception(f'Table {bold(table_name)} found in SQLite but not accounted for in {path("table_info.json")}')
+
+        for table_name in table_info.keys():
+            if table_name not in sqlite_table_names:
+                raise Exception(f'Table {bold(table_name)} found in {path("table_info.json")} but not in SQLite')
+
+        # Extract all rows for all tables in chat.db
+        self.logger.info('Reading chat.db source table metadata')
+        table_objects = {}
+        for table_name, table_data in table_info.items():
+            table_object = ChatDbTable(sqlite_con=self.sqlite_con,
+                                       table_name=table_name,
+                                       write_mode=table_data['write_mode'],
+                                       primary_key=table_data['primary_key'],
+                                       pg_schema=self.pg_schema)
+
+            table_objects[table_name] = table_object
+            self.logger.info(pydoni.advanced_strip(f"""
+            Read SQLite:{click.style(table_name, bold=True)},
+            shape: {table_object.shape},
+            primary key: {str(table_data['primary_key'])}"""), arrow='white')
+
+        return table_objects
+
+    def save_to_csv(self, dir_name: str, logger: logging.Logger) -> None:
         """
-        Save all tables to csv files.
+        Save all tables to .csv files.
         """
         for table_name, table_object in self.table_objects.items():
             ext = '.csv'
             output_file_name = join(dir_name, table_name + ext)
             table_object.save_to_csv(output_file_name)
-            if verbose:
-                file_size_str = pydoni.human_filesize(stat(output_file_name).st_size)
-                logger.info(f'Saved table {bold(table_name)} to {bold(table_name + ext)} ({file_size_str})', arrow='white')
+            file_size_str = pydoni.human_filesize(stat(output_file_name).st_size)
+            logger.info(f'Saved table {bold(table_name)} to {bold(table_name + ext)} ({file_size_str})', arrow='white')
 
-
-def extract_sqlite(logger, sqlite_con: sqlite3.Connection) -> ChatDbExtract:
-    """
-    Query SQLite database for iMessage tables, and filter for records not already in
-    the Postgres database mirrored tables.
-
-    For example if this workflow was run 1hr ago, only extract the last hour's worth of
-    iMessage data from SQLite, rather than for all time.
-    """
-    # Connect to SQLite chat.db
-    sqlite_cursor = sqlite_con.cursor()
-
-    # Get full list of SQLite tables in DB
-    sqlite_cursor.execute("select name from sqlite_master where type = 'table';")
-    sqlite_tables = [x[0] for x in sqlite_cursor.fetchall()]
-
-    chat_db_extract = ChatDbExtract()
-
-    # Extract all rows for all tables in chat.db
-    logger.info('Reading chat.db source tables')
-    for tname in [x for x in sqlite_tables]:
-        df_sqlite = pd.read_sql(f'select * from {tname}', sqlite_con)
-        tobject = ChatDbTable(df_sqlite, sqlite_table_name=tname)
-        chat_db_extract.add_table(tname, tobject)
-        logger.info(f"Read SQLite:{click.style(tname, bold=True)}, shape: {df_sqlite.shape}", arrow='white')
-
-    logger.info('iMessage data successfully extracted from chat.db ✔️', bold=True)
-    return chat_db_extract
+    def save_to_postgres(self, pg: Postgres, pg_schema: str, logger: logging.Logger) -> None:
+        """
+        Save all tables to Postgres.
+        """
+        for table_name, table_object in self.table_objects.items():
+            table_object.save_to_postgres(pg=pg, pg_schema=pg_schema)
+            logger.info(f'Saved Postgres:{bold(self.pg_schema + "." + table_name)}', arrow='white')
