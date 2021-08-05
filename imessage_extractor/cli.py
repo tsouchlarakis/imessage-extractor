26945bbef722e1e05b8265d3fbbb3c9adac93aa1
Andoni Sooklaris <andonis@apple.com>
2021-08-04 19:02:17 -0500

    refresh_contacts

--- imessage_extractor/cli.py
+++ imessage_extractor/cli.py
@@ -1,6 +1,7 @@
 import logging
 import click
 from .src.go import go
+from .src.refresh_contacts import refresh_contacts
 
 
 @click.group()
@@ -12,6 +13,7 @@ def cli():
 
 
 cli.add_command(go)
+cli.add_command(refresh_contacts)
 
 
 def main(args=None):
