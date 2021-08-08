7ce8b8a83e0b98f784fe60dc5908992e42da6ff4
Andoni Sooklaris <andonis@apple.com>
2021-08-08 16:30:32 -0500

    Added is_emoji flag

--- imessage_extractor/src/tables/staging/definitions/tokens.py
+++ imessage_extractor/src/tables/staging/definitions/tokens.py
@@ -1,11 +1,12 @@
 import logging
+
+import emoji
 import nltk
 import pydoni
 import string
-from nltk.corpus import stopwords
-import typing
 from ....verbosity import bold
 from ..common import columns_match_expectation
+from nltk.corpus import stopwords
 
 
 def is_punctuation(token):
@@ -15,18 +16,24 @@ def is_punctuation(token):
     return token in string.punctuation + '’‘“”``'
 
 
+def is_emoji(token):
+  """
+  Determine whether a token is an emoji.
+  """
+
+
 def refresh_tokens(pg: pydoni.Postgres,
                    pg_schema: str,
                    table_name: str,
                    columnspec: dict,
-                   references: typing.Union[list, None],
                    logger: logging.Logger):
     """
     Map each unique token to descriptor columns (stem, lemma, length, language, etc.).
     """
-    logger.info(f'Refreshing staging table "{bold(pg_schema)}"."{bold(table_name)}"')
+    logger.info(f'Refreshing staging table "{bold(pg_schema)}"."{bold(table_name)}"', arrow='white')
 
     message_tokens_table_name = 'message_tokens'
+    emoji_text_map_table_name = 'emoji_text_map'
 
     if pg.table_exists(pg_schema, table_name):
       # Filter out messages that are already in the message <> emoji mapping table if
@@ -43,7 +50,10 @@ def refresh_tokens(pg: pydoni.Postgres,
     from {pg_schema}.{message_tokens_table_name} m
     {join_clause}
     """, simplify=False)  # Returns a dataframe with one column
-    logger.info(f'Gathering token data for {len(new_tokens)} new, unique tokens')
+    logger.debug(f'Gathering information by token for {len(new_tokens)} new, unique tokens')
+
+    emoji_text_map = pg.read_table(pg_schema, emoji_text_map_table_name)[['emoji']]
+    emoji_text_map['is_emoji'] = True
 
     if len(new_tokens) > 0:
         stops = stopwords.words('english')
@@ -60,9 +70,20 @@ def refresh_tokens(pg: pydoni.Postgres,
 
         for col, fun in column_apply_function_map.items():
             new_tokens[col] = new_tokens['token'].apply(fun)
+            logger.debug(f'Applied aggregations to column {bold(col)}')
 
-    columns_match_expectation(new_tokens, table_name, columnspec)
-    new_tokens.to_sql(table_name, pg.dbcon, schema=pg_schema, index=False, if_exists='append')
+        # Add emoji information
+        new_tokens = new_tokens.merge(emoji_text_map[['emoji']], left_on='token', right_on='emoji', how='left')
+        new_tokens['is_emoji'].fillna(False, inplace=True)
 
-    n_new_tokens_str = len(new_tokens) if len(new_tokens) > 0 else 'no'
-    logger.info(f'Built "{bold(pg_schema)}"."{bold(table_name)}", shape: {new_tokens.shape}')
+        columns_match_expectation(new_tokens, table_name, columnspec)
+        new_tokens.to_sql(name=table_name,
+                          con=pg.dbcon,
+                          schema=pg_schema,
+                          index=False,
+                          if_exists='append')
+
+        logger.info(f'Built "{bold(pg_schema)}"."{bold(table_name)}", shape: {new_tokens.shape}')
+
+    else:
+        logger.info(f'No new tokens to add to "{bold(pg_schema)}"."{bold(table_name)}"', arrow='white')
