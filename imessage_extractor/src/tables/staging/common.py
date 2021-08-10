20379b891094de245e55106bdc470ed89747257c
Andoni Sooklaris <andonis@apple.com>
2021-08-10 08:07:04 -0500

    Updated error string

--- imessage_extractor/src/tables/staging/common.py
+++ imessage_extractor/src/tables/staging/common.py
@@ -14,10 +14,11 @@ def columns_match_expectation(df: pd.DataFrame, table_name: str, columnspec: dic
 
     for col in expected_columns:
         assert col in actual_columns, \
-            pydoni.advanced_strip(f"""'Column {bold(col)} defined in staging_table_info.json
-            for table {bold(table_name)} but not in actual dataframe columns' """)
+            pydoni.advanced_strip(f"""Column {bold(col)} defined in staging_table_info.json
+            for table {bold(table_name)} but not in actual dataframe columns
+            ({bold(str(df.columns))})""")
 
     for col in actual_columns:
         assert col in expected_columns, \
-            pydoni.advanced_strip(f"""'Column {bold(col)} in actual dataframe {bold(table_name)}
-            columnspec but not in staging_table_info.json""")
+            pydoni.advanced_strip(f"""Column {bold(col)} in actual dataframe {bold(table_name)}
+            columns ({bold(str(df.columns))}) but not in staging_table_info.json""")
