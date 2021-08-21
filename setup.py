ac0062b19d760852224be4db6af078d893018bf1
Andoni Sooklaris <andonis@apple.com>
2021-08-21 15:36:19 -0500

    Test

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ from setuptools import setup, find_packages
 
 with open('README.rst') as readme_file:
     readme = readme_file.read()
+    #test
 
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
