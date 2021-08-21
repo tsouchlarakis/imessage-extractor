eabd5254f552c5541760db4b70ab3937e16c7993
Andoni Sooklaris <andonis@apple.com>
2021-08-21 15:26:13 -0500

    Removed long_description

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
     },
     install_requires=requirements,
     license='MIT license',
-    long_description=readme + '\n\n' + history,
+    # long_description=readme + '\n\n' + history,
     include_package_data=True,
     keywords='imessage_extractor',
     name='imessage_extractor',
