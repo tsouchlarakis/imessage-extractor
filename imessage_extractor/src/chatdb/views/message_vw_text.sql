9c263e1fd275fd20568e92aa8597f8f0dba8cbc1
Andoni Sooklaris <andonis@apple.com>
2021-08-24 22:10:25 -0500

    Added message_vw_text

---
+++ imessage_extractor/src/chatdb/views/message_vw_text.sql
@@ -0,0 +1,31 @@
+drop view if exists {pg_schema}.message_vw_text;
+create view {pg_schema}.message_vw_text as
+
+select message_id
+       , chat_identifier
+       , contact_name
+       , ts
+       , dt
+       , "text"
+       , length(case when is_emote = false and is_url = false and is_empty = false and message_special_type is null
+                          then "text"
+                     else null
+                end) as n_characters
+       , case when is_emote = false and is_url = false and message_special_type is null
+                   then array_length(regexp_split_to_array("text", '\s+'), 1)
+              when is_empty
+                   then 0
+              else null
+         end as n_tokens
+       , service
+       , is_from_me
+       , is_group_chat
+       , message_special_type
+       , is_thread
+       , thread_original_message_id
+       , has_attachment
+from {pg_schema}.message_vw
+where is_text = true
+  and is_empty = false
+
+
